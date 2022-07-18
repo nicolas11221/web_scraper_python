33ce5ed5300f86c58de16b7ee134d37e82307deb
Jean Carlos A <jeanc.developer@outlook.com>
2022-07-18 00:41:52 -0400

    fix(name): Fix variable name & add new element

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
 from services.scraper import get_html, get_data, data_handler
 
-url = [{
+urls = [{
     "url": 'https://simple.ripley.cl/tecno/celulares?source=menu&s=mdco',
     "element1": 'div',
     "element_class1": 'catalog-product-details__name',
     "element2": 'li',
     "element_class2": 'catalog-prices__offer-price',
-},]
+},{
+    "url": "",
+    "element1":"",
+    "element_class1":"",
+    "element2":"",
+    "element_class2":"",
+}]
 
 def main(url):
-    for i in url:
-        html = get_html(i['url'])
-        cel = get_data(html, i['element1'], i['element_class1'])
-        price = get_data(html, i['element2'], i['element_class2'])
+    for url in urls:
+        html = get_html(url['url'])
+        cel = get_data(html, url['element1'], url['element_class1'])
+        price = get_data(html, url['element2'], url['element_class2'])
         celulares = data_handler(cel)
         precios = data_handler(price)
         print(celulares, precios)
 
-main(url)
+main(urls)
